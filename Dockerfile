apconole/stap

commit 6c362768405aaf8db315c47d55e83133c288026c

Added bzip2 so that perf archive works

Dockerfile
@@ -21,6 +21,7 @@ RUN echo '[rhel8-hack]' > /etc/yum.repos.d/hack.repo && \
     perf \
     python38 \
     strace \
+    bzip2
     systemtap \
     tcpdump \
   && yum --disableplugin=subscription-manager --setopt=tsflags=nodocs -y install \

commit b16a294f76e8a22e34ccbeadc607d071cf25def7

Dockerfile: Add missing line-continuation
Previous commit accidentally omitted the line continuation when adding
'bzip2' package.  Add it back in.

Fixes: 6c362768405a ("Added bzip2 so that perf archive works")
Signed-off-by: Aaron Conole <aconole@redhat.com>

Dockerfile
@@ -21,7 +21,7 @@ RUN echo '[rhel8-hack]' > /etc/yum.repos.d/hack.repo && \
     perf \
     python38 \
     strace \
-    bzip2
+    bzip2 \
     systemtap \
     tcpdump \
   && yum --disableplugin=subscription-manager --setopt=tsflags=nodocs -y install \